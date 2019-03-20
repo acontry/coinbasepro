4cb71b68fb170b3029366cfdae26506eb734c095
Alex Contryman <acontry@gmail.com>
2019-03-20 15:40:36 -0700

    Move authorization class to separate file

---
+++ coinbasepro/auth.py
@@ -0,0 +1,35 @@
+import time
+
+import base64
+import hashlib
+import hmac
+from requests.auth import AuthBase
+
+
+class CoinbaseProAuth(AuthBase):
+    """Request authorization.
+
+    Provided by Coinbase Pro:
+    https://docs.pro.coinbase.com/?python#signing-a-message
+    """
+    def __init__(self, api_key, secret_key, passphrase):
+        self.api_key = api_key
+        self.secret_key = secret_key
+        self.passphrase = passphrase
+
+    def __call__(self, request):
+        timestamp = str(time.time())
+        message = (timestamp + request.method + request.path_url +
+                   (request.body or ''))
+        message = message.encode('ascii')
+        hmac_key = base64.b64decode(self.secret_key)
+        signature = hmac.new(hmac_key, message, hashlib.sha256)
+        signature_b64 = base64.b64encode(signature.digest())
+        request.headers.update({
+            'CB-ACCESS-SIGN': signature_b64,
+            'CB-ACCESS-TIMESTAMP': timestamp,
+            'CB-ACCESS-KEY': self.api_key,
+            'CB-ACCESS-PASSPHRASE': self.passphrase,
+            'Content-Type': 'application/json'
+        })
+        return request
